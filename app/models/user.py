7bb2c3bfb7442c0739a9144ee47b845e695ffab5
Mahmud Jewel <jmbengal.mbstu@gmail.com>
2024-02-18 03:51:33 -0500

    Adding extran info on the model

--- app/models/user.py
+++ app/models/user.py
@@ -16,5 +16,8 @@ class User(CommonModel):
 	last_name = Column(String(50), nullable=True)
 	role = Column(Enum(UserRole), default=UserRole.user)
 
+	def __repr__(self):
+		return f"{self.email}"
+	
 metadata = Base.metadata
 
