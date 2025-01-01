73969da63c718ef80c7b2c75df6206d239cd8659
Mahmud Jewel <jmbengal.mbstu@gmail.com>
2025-01-02 01:17:48 +0600

    Added global constant

--- app/models/user.py
+++ app/models/user.py
@@ -1,11 +1,13 @@
 from sqlalchemy import Column, String, Enum
 from enum import Enum as PythonEnum
 from app.core.database import Base
-from .common import CommonModel
 
-class UserRole(str, PythonEnum):
-	user = "user"
-	admin = "admin"
+from .common import CommonModel
+from app.utils.constant.globals import UserRole
+
+# class UserRole(str, PythonEnum):
+# 	user = "user"
+# 	admin = "admin"
 
 class User(CommonModel):
 	__tablename__ = "users"
@@ -14,7 +16,7 @@ class User(CommonModel):
 	password = Column(String(50), nullable=True) # nullable=True for social auth(OAuth2)
 	first_name = Column(String(50), nullable=True)
 	last_name = Column(String(50), nullable=True)
-	role = Column(Enum(UserRole), default=UserRole.user)
+	role = Column(Enum(UserRole), default=UserRole.USER)
 
 	def __repr__(self):
 		return f"{self.email}"
